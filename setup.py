b2e09a1d84eb67cf2c9680e91caef6983e52f0d5
Felipe Fagundes <fagundesdeveloper@gmail.com>
2024-06-03 21:16:38 -0300

    change setup

--- setup.py
+++ setup.py
@@ -11,8 +11,8 @@ setup(
     description="Time-Accurate Automatic Speech Recognition using Whisper.",
     readme="README.md",
     python_requires=">=3.8",
-    author="Max Bain",
-    url="https://github.com/m-bain/whisperx",
+    author="Felipe Fagundes",
+    url="https://github.com/fagundesdeveloper/whisperx",
     license="MIT",
     packages=find_packages(exclude=["tests*"]),
     install_requires=[
