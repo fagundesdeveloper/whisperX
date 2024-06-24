818371463437769888cc0452ffe5ae808845f855
Felipe Fagundes <fagundesdeveloper@gmail.com>
2024-06-24 14:20:54 -0300

    revert to o

--- whisperx/vad.py
+++ whisperx/vad.py
@@ -198,7 +198,7 @@ class Binarize:
 class VoiceActivitySegmentation(VoiceActivityDetection):
     def __init__(
         self,
-        segmentation: PipelineModel = "filfagundes/speaker-segmentation-fine-tuned-pt",
+        segmentation: PipelineModel = "pyannote/segmentation",
         fscore: bool = False,
         use_auth_token: Union[Text, None] = None,
         **inference_kwargs,
